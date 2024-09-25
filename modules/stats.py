1150490a5c2aca51f134ebced0969ee1e15a0c43
Anthony Therrien <anthony.therrien@usherbrooke.ca>
2024-09-24 21:06:51 -0400

    Refactor 3

--- modules/stats.py
+++ modules/stats.py
@@ -1,26 +1,3 @@
-VALID_STATS = [
-    'WorkSpeedGlobal', 'ShootingAccuracyPawn', 'MeleeHitChance', 'MoveSpeed', 'AimingDelayFactor',
-    'PainShockThreshold', 'GlobalLearningFactor', 'MedicalTendSpeed',
-    'MedicalTendQuality', 'MedicalSurgerySuccessChance', 'ResearchSpeed', 'SocialImpact',
-    'NegotiationAbility', 'TradePriceImprovement', 'ConstructionSpeed', 'MiningYield',
-    'PlantWorkSpeed', 'ButcheryFleshSpeed', 'ButcheryFleshEfficiency', 'ButcheryMechanoidSpeed',
-    'ButcheryMechanoidEfficiency', 'CookSpeed', 'EatSpeed', 'ImmunityGainSpeed',
-    'TameAnimalChance', 'TrainAnimalChance', 'PawnBeauty', 'RestRateMultiplier', 'HungerRate',
-    'PsychicSensitivity', 'PsychicEntropyRecoveryRate', 'MeditationFocusGain', 'SocialImpact',
-    'AnimalGatherYield', 'AnimalProductYield', 'ConstructionSpeed',
-    'ConstructSuccessChance', 'SurgerySuccessChanceFactor', 'HealingSpeed', 'FoodPoisonChance',
-    'CarryWeight', 'ShootingAccuracyPawn', 'CaravanRidingSpeed', 'BedRestEffectiveness',
-    'Beauty', 'PainFactor', 'MentalBreakThreshold', 'EatingSpeed', 'Talking', 'NegotiationAbility',
-    'TradePriceImprovement', 'MeleeDodgeChance', 'RestFallFactor',
-    'FoodPoisonChance', 'MeleeDPS', 'MeleeDodgeChance', 'ArrestSuccessChance',
-    'ArmorRating_Sharp', 'ArmorRating_Blunt', 'MedicalOperationSpeed', 'MaxNutrition'
-]
-
-VALID_CAPACITIES = [
-    'Consciousness', 'Sight', 'Hearing', 'Moving', 'Manipulation', 'Talking', 'Eating', 'Breathing',
-    'BloodPumping', 'BloodFiltration', 'Metabolism', 'Immunity'
-]
-
 class Stat:
     def __init__(self, name, value, stat_type='stat', mod_type='factor'):
         self.name = name
